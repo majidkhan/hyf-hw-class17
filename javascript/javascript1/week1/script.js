57aa4970e2c4793887d5a6b21f749de142f16647
majidkhan <majidkhan_l@hotmail.com>
2021-03-10 13:00:20 +0100

    adding index and js file to week1 hw

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,8 @@
+
+let yearOfBirth = 1983;
+let yearFuture = 2050;
+let age = yearFuture - yearOfBirth;
+
+let outPut = 'You will be ' + age + ' years old ' + 'in ' + yearFuture;
+
+console.log(outPut);
